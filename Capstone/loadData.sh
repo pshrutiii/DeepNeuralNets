166df12a5e697bbab7aabda02ad079c62803cb1a
pshrutiii <contactdevshruti@gmail.com>
2018-03-25 10:43:20 -0700

    refactor: shell script to speed things up

---
+++ Capstone/loadData.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+ 
+sudo python3 -m pip install -r requirements.txt
+cat <<EOF >/home/ubuntu/.kaggle/kaggle.json
+{"username":"pshrutiii","key":"25c27698e3f903dc046962eea76878b5"}
+EOF
+cd /home/ubuntu/.kaggle
+chmod 600 kaggle.json
+kaggle competitions download -c landmark-retrieval-challenge
+cd /home/ubuntu/.kaggle/competitions/landmark-retrieval-challenge
+unzip index.csv.zip
+unzip sample_submission.csv.zip
+unzip test.csv.zip
+mv index.csv ~/DeepNeuralNets/Capstone/.
+mv sample_submission.csv ~/DeepNeuralNets/Capstone/.
+mv test.csv ~/DeepNeuralNets/Capstone/.
