e42ecb01064948ab391f9f94bf3f207e6f64ad86
pshrutiii <contactdevshruti@gmail.com>
2018-03-25 10:56:08 -0700

    fix: shell script

--- Capstone/loadData.sh
+++ Capstone/loadData.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
  
 sudo python3 -m pip install -r requirements.txt
+cd /home/ubuntu/.kaggle
 cat <<EOF >/home/ubuntu/.kaggle/kaggle.json
 {"username":"pshrutiii","key":"25c27698e3f903dc046962eea76878b5"}
 EOF
-cd /home/ubuntu/.kaggle
 chmod 600 kaggle.json
 kaggle competitions download -c landmark-retrieval-challenge
 cd /home/ubuntu/.kaggle/competitions/landmark-retrieval-challenge
